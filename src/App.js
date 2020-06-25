002e3f7b6bedb49b27a1d139b2860062a309365c
tsutomu-noguchi <50165379+tsutomu-noguchi@users.noreply.github.com>
2020-06-25 20:12:00 +0900

    lesson2 useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React from 'react';
-// カスタムフック
+import React, { useState, useEffect } from 'react';
+// カスタムフック読み込み
 import { useForm } from './hooks/useForm';
+import { useFetch } from './hooks/useFetch';
+
 import './App.css';
 
 const App = () => {
 	const [values, handleChange] = useForm({ email: '', password: '' });
 
+	const [count, setCount] = useState(() => JSON.parse(localStorage.getItem('count')) ?? 1);
+	const { data, loading } = useFetch(`http://numbersapi.com/${count}/trivia`);
+
+	// localStorageにcountを保存
+	useEffect(() => {
+		localStorage.setItem('count', JSON.stringify(count));
+	}, [count]);
+
+	// useEffect(() => {
+	// 	console.log('rendering useEffect');
+	//   // cleanUp
+	// 	return () => {
+	// 		console.log('unmount App');
+	// 	};
+	// }, []);
+
 	return (
 		<div>
+			<div>{!data ? 'loading...' : data}</div>
+			<div>count: {count}</div>
+			<button onClick={() => setCount((c) => c + 1)}>Increment</button>
 			<label for="email">E-mail</label>
 			<input name="email" value={values.email} onChange={handleChange} />
 			<label for="password">Password</label>
